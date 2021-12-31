b0c32ac06a0ef1fcd5d99423fba396265d555f6e
olayode <aolayode@getcarbon.co>
2021-12-31 10:22:03 +0100

    project setup

---
+++ src/test/java/com/possible/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.possible.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
